b2ac7ad1b2cbb7d9d714e6333a5899dd0733990c
Shane Bryldt <shane@signalwire.com>
2020-01-10 15:07:31 -0700

    Added some plumbing for Managed API's

--- Loader/Loader.cs
+++ Loader/Loader.cs
@@ -1,37 +1,99 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Runtime.InteropServices;
 using FreeSWITCH;
 
 namespace FreeSWITCH
 {
-    public sealed class Loader
+    public static class Loader
     {
+	// The primary entry point delegate for obtaining the native callbacks during host initialization
         private delegate NativeCallbacks LoadDelegate();
 
-	private delegate int APICallback(string command, IntPtr sessionptr, IntPtr streamptr);
+	// The FreeSWITCH API interface callback delegate
+	private delegate int NativeAPICallback(string command, IntPtr sessionptr, IntPtr streamptr);
 
+	// Contains all native callbacks that will be called from the native host, each callback
+	// is produced by marshalling delegates to native function pointers
+	// Important: Must maintain the same structure in native_callbacks_t in mod_coreclr.c
         [StructLayout(LayoutKind.Sequential)]
         private struct NativeCallbacks
         {
-	    public IntPtr APICallback;
+	    public IntPtr NativeAPICallback;
         }
-	
+
+	// This is the only predefined entry point, this must match what mod_coreclr.c is looking for
         private static NativeCallbacks Load()
         {
+	    sAPICallbacks.TryAdd("test", TestAPI);
             return new NativeCallbacks {
-		APICallback = Marshal.GetFunctionPointerForDelegate<APICallback>(APIHandler)
+		NativeAPICallback = Marshal.GetFunctionPointerForDelegate<NativeAPICallback>(NativeAPIHandler)
 	    };
         }
 
-	private static int APIHandler(string command, IntPtr sessionptr, IntPtr streamptr)
+	// The Managed API interface callback delegate
+	public delegate bool APICallback(string args, ref string result);
+	private static ConcurrentDictionary<string, APICallback> sAPICallbacks = new ConcurrentDictionary<string, APICallback>();
+	
+	// This is the FreeSWITCH API interface callback handler which is bound to "coreclr" API commands
+	private static int NativeAPIHandler(string command, IntPtr sessionptr, IntPtr streamptr)
 	{
 	    using ManagedSession session = new ManagedSession(new SWIGTYPE_p_switch_core_session_t(sessionptr, false));
 	    using Stream stream = new Stream(new SWIGTYPE_p_switch_stream_handle_t(streamptr, false));
 
-	    Log.WriteLine(LogLevel.Info, "Managed API: {0}", command);
+	    string args = command;
+	    if (!ParseArgument(ref args, out command, ' '))
+	    {
+	        Log.WriteLine(LogLevel.Error, "Missing Managed API");
+		stream.Write("-ERROR Missing Managed API");
+		return 1;
+	    }
+	    Log.WriteLine(LogLevel.Info, "Managed API: {0} {1}", command, args);
 
-	    stream.Write("+WOOT");
-	    return 0;
+	    if (!sAPICallbacks.TryGetValue(command.ToLower(), out APICallback callback))
+	    {
+		Log.WriteLine(LogLevel.Error, "Managed API does not exist");
+		stream.Write("-ERROR Managed API does not exist");
+		return 1;
+	    }
+	    string result = null;
+	    bool succeeded = false;
+	    try
+	    {
+	    	succeeded = callback(args, ref result);
+	    }
+	    catch (Exception)
+	    {
+	        Log.WriteLine(LogLevel.Error, "Managed API Exception");
+		// TODO: Write more of the exception data out
+	    }
+	    if (result != null) stream.Write(result);
+	    return succeeded ? 0 : 1;
+	}
+
+	// TODO: Put this somewhere more reusable
+	private static bool ParseArgument(ref string args, out string arg, char separator)
+	{
+	    args = args.Trim();
+	    int eoa = args.IndexOf(separator);
+	    arg = null;
+	    if (eoa < 0)
+	    {
+		string tmp = args;
+		args = string.Empty;
+		if (tmp.Length > 0) arg = tmp;
+	        return arg != null;
+	    }
+	    arg = args.Substring(0, eoa);
+	    args = args.Remove(0, eoa + 1).Trim();
+	    return true;
+	}
+
+	private static bool TestAPI(string args, ref string result)
+	{
+	    Log.WriteLine(LogLevel.Info, "Managed TestAPI Executed");
+	    result = "+WOOHOO";
+	    return true;
 	}
     }
 }
