16bee6f2f266d9961a9c05d0686223c00407fd20
Dave Kompel <drk@drkngs.net>
2020-02-11 13:55:16 -0500

    Add the plugininterface project and sln for building

---
+++ PluginInterface/IPluginDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginInterface
+{
+    public interface IPluginDispatcher
+    {
+        bool Onload();
+
+        void DispatchDialPlan();
+
+        void DispatchAPI();
+
+        IEnumerable<string> GetApiNames();
+
+        IEnumerable<string> GetDPNames();
+    }
+}
