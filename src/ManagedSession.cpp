65ded2bdc8710b90c47d8976540329772268c05d
Dave Kompel <drk@drkngs.net>
2020-07-10 18:48:10 -0400

    Getting there

--- src/ManagedSession.cpp
+++ src/ManagedSession.cpp
@@ -39,11 +39,10 @@ void ManagedSession::check_hangup_hook()
 
 switch_status_t ManagedSession::run_dtmf_callback(void* input, switch_input_type_t itype)
 {
-	ATTACH_THREADS
-		if (cb_status.function == NULL || itype != switch_input_type_t.SWITCH_INPUT_TYPE_DTMF) {
+		if (cb_state.function == NULL || itype != switch_input_type_t.SWITCH_INPUT_TYPE_DTMF) {
 			return SWITCH_STATUS_FALSE;;
 		}
-	char* result = ((inputFunction)cb_status.function)((char*)input);
+	char* result = ((inputFunction)cb_state.function)((char*)input);
 	switch_status_t status = process_callback_result(result);
 
 	RESULT_FREE(result);
